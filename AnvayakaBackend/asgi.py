140de7790a3ca17c765634f65e522fad15147bbe
shivankupadhyay-eng <shivankupadhyay51@gmail.com>
2025-08-21 03:54:48 +0530

    fixing channels import error

--- AnvayakaBackend/asgi.py
+++ AnvayakaBackend/asgi.py
@@ -12,10 +12,9 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import backend.routing
+from channels.layers import get_channel_layer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AnvayakaBackend.settings')
-
-from channels.layers import get_channel_layer
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
